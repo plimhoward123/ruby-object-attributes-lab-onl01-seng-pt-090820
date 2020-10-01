7bed5da9aeee5cb438b89e4b87b9a593667a50c6
Plimon Howard V <plimon.howard@gmail.com>
2020-10-01 22:09:49 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,24 @@
+class Dog
+  def name=(dog_name)
+    @name = dog_name
+  end
+
+  def name
+    @name
+  end
+
+  def breed=(breed_type)
+    @breed = breed_type
+  end
+
+  def breed
+    @breed
+  end
+
+end
+
+
+doge = Dog.new
+
+doge.instance_variable_set(:@name, "Fido")
+doge.instance_variable_set(:@breed, "Beagle")
---
+++ lib/person.rb
@@ -0,0 +1,22 @@
+class Person
+
+  def name=(person_name)
+    @name = person_name
+  end
+  def name
+    @name
+  end
+  def job=(job)
+    @job = job
+  end
+  def job
+    @job
+  end
+
+
+end
+
+person = Person.new
+
+person.instance_variable_set(:@name, "Beyonce")
+person.instance_variable_set(:@job, "Singer")
